a43df412e0c23bd2659ee9ec2aee657dd730c307
riad <aourti_r@etna-alternance.net>
2023-10-16 16:19:13 +0200

    bl

---
+++ boucle.js
@@ -0,0 +1,28 @@
+// creer un escalier
+let nbrclm = 0;
+const  b = 10;
+let cae = "#";
+let nbrlign = 0;
+const n = 10;
+let lae= "#";
+
+//tant que nb < n
+while(nbrclm< b){
+   // rajouter # a colonne a ecrire 
+   console.log(cae);
+   // rajouter # a colonne a ecrire
+   nbrclm++;
+   cae += "#";
+}
+//alors tant que nb#< nbligne a ecrire
+//alors   ecrire #
+// rajouter # a ligne a ecrire
+
+while (nbrlign < n){
+    console.log (lae);
+    //
+    nbrlign++;
+    //
+    lae += "#";
+
+}
