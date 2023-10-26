f2f21788c2e73bbf5e3ea549b15cd6bba4d25459
alexandre solbes <alexandresolbes@gmail.com>
2023-10-26 16:32:58 +0200

    design of checkout page finished

--- checkout.js
+++ checkout.js
@@ -1,10 +1,8 @@
-function selectDiv(card_product) {
-  let selectedDiv =
-  document.getElementsByClassName(card_product);
 
-  if (selectedDiv) {
-    selectedDiv.style.border =
-  'blue';
-  console.log(selectedDiv)
-  }
+
+function selectDiv(card_product) {
+  let selectedDiv = document.querySelectorAll(card_product)
+  selectedDiv.forEach(element => {
+
+  });
 }
