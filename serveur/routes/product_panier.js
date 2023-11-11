ec2e4930bfbb16ef2add2a81d6e3b3c4e6a9f771
riad <aourti_r@etna-alternance.net>
2023-11-11 21:58:00 +0100

    toutes les routes

--- serveur/routes/product_panier.js
+++ serveur/routes/product_panier.js
@@ -2,10 +2,12 @@ import Express from 'express';
 
 // instanciation du router d'express
 const routes = Express.Router();
-import {getProductById, addProduct, updateProduct, getAllProducts }from '../controllers/ProductPanierController.js';
+import {getProductById, addProduct, updateProduct, deleteProduct, getAllProducts }from '../controllers/ProductPanierController.js';
 
 routes.get('/allproducts', getAllProducts)
 routes.get('/:idProduct', getProductById)
 routes.post('/newProduct', addProduct)
 routes.patch('/updateProduct/:idProduct', updateProduct)
+routes.delete('/deleteProduct/:idProduct', deleteProduct)
+
 export default routes;
