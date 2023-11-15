56a3799b4b74fdac4d8cbeb2084981beb67338c1
adelinelucas <82570582+adelinelucas@users.noreply.github.com>
2023-11-15 13:56:00 +0100

    update html file in public folder and add images in public folder

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/Navbar/Navbar.js';
 import Catalogue from './pages/Catalogue';
 import Checkout from './pages/Checkout/Checkout';
 import Panier from './pages/Panier/Panier';
