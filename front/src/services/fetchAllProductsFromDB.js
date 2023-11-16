d5ba9a615e0310474bf085e7bc753f741d46d674
adelinelucas <82570582+adelinelucas@users.noreply.github.com>
2023-11-16 15:06:59 +0100

    productlist component and bestseller, fetch data from api

--- front/src/services/fetchAllProductsFromDB.js
+++ front/src/services/fetchAllProductsFromDB.js
@@ -1,5 +1,9 @@
 export async function fetchAllProductsFromDB(url) {
-    return await fetch(url).then((response) =>
-        response.json()
-    );
+    try{
+        const response = await fetch(url);
+        const data = await response?.json();
+        return(data.message)
+    }catch(error){
+        return console.log(error)
+    }
 }
