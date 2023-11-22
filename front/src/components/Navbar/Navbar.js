3a4664bd2835382b6528bb69f58ea0d23674f48f
alexandre solbes <alexandresolbes@gmail.com>
2023-11-22 15:31:13 +0100

    fixed navbar when scrolling

--- front/src/components/Navbar/Navbar.js
+++ front/src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className="navbar" style={{backgroundColor:"var(--light-blue-ara)", textDecoration:"none", justifyContent:"space-evenly", width:"100%"}}>
+        <nav className="navbar" style={{backgroundColor:"var(--light-blue-ara)", textDecoration:"none", justifyContent:"space-evenly", width:"100%",position: 'sticky', top: 0, zIndex: 100}}>
             <NavLink style={{textDecoration:"none", color:"white", fontWeight:"bold"}} to={'/'}>Home</NavLink>
             <NavLink style={{textDecoration:"none", color:"white", fontWeight:"bold"}} to={'/catalogue'}>Catalogue</NavLink>
             <NavLink style={{textDecoration:"none", color:"white", fontWeight:"bold"}} to={'/panier'}>Panier</NavLink>
