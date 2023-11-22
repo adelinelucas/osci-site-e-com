75520ccd33b2bf410f38e4edda397e224cb10b3b
alexandre solbes <alexandresolbes@gmail.com>
2023-11-22 15:38:22 +0100

    fixed your cart part along with stickt navbar

--- front/src/components/Navbar/Navbar.js
+++ front/src/components/Navbar/Navbar.js
@@ -3,11 +3,11 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className="navbar" style={{backgroundColor:"var(--light-blue-ara)", textDecoration:"none", justifyContent:"space-evenly", width:"100%",position: 'sticky', top: 0, zIndex: 100}}>
-            <NavLink style={{textDecoration:"none", color:"white", fontWeight:"bold"}} to={'/'}>Home</NavLink>
-            <NavLink style={{textDecoration:"none", color:"white", fontWeight:"bold"}} to={'/catalogue'}>Catalogue</NavLink>
-            <NavLink style={{textDecoration:"none", color:"white", fontWeight:"bold"}} to={'/panier'}>Panier</NavLink>
-            <NavLink style={{textDecoration:"none", color:"white", fontWeight:"bold"}} to={'/checkout'}>Checkout</NavLink>
+        <nav className="navbar" style={{backgroundColor:"var(--light-blue-ara)", textDecoration:"none", justifyContent:"space-evenly", width:"100%",position: 'sticky', top: 0, zIndex: 1000}}>
+            <NavLink style={{textDecoration:"none", color:"white", fontWeight:"bold"}} to={'/'}>🏠 Home</NavLink>
+            <NavLink style={{textDecoration:"none", color:"white", fontWeight:"bold"}} to={'/catalogue'}>📚 Catalogue</NavLink>
+            <NavLink style={{textDecoration:"none", color:"white", fontWeight:"bold"}} to={'/panier'}>🛒 Cart</NavLink>
+            <NavLink style={{textDecoration:"none", color:"white", fontWeight:"bold"}} to={'/checkout'}>✅ Checkout</NavLink>
         </nav>
     );
 };
